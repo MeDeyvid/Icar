84d0e00f55c942311da76eec20e6fd218eb970e4
kanydian <kanydianestevescanedo@yahoo.com>
2021-10-25 21:09:54 -0300

    Construindo ambiente para desenvolvimento front-end.

---
+++ view/icar/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Login/>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000;
+  margin-top: 60px;
+}
+</style>
---
+++ view/icar/src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="tit">
+    <h1>Icar</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
